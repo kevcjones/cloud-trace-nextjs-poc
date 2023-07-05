e3b29cba4617ad67d6c101774c59d5e614e62cb7
Kevin Jones <kevijones@ebay.com>
2023-07-05 11:29:03 +0100

    remove always sampler

--- instrumentation-node.ts
+++ instrumentation-node.ts
@@ -4,22 +4,18 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
-import { AlwaysOnSampler, BatchSpanProcessor } from '@opentelemetry/sdk-trace-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node'
+import { TraceExporter } from "@google-cloud/opentelemetry-cloud-trace-exporter";
 
 const sdk = new NodeSDK({
-    sampler: new AlwaysOnSampler(),
+    // sampler: new AlwaysOnSampler(),
     resource: new Resource({
         [SemanticResourceAttributes.SERVICE_NAME]: 'next-app',
     }),
     instrumentations: [getNodeAutoInstrumentations({
-        '@opentelemetry/instrumentation-http': {
-            applyCustomAttributesOnSpan: (span) => {
-                span.setAttribute('foo2', 'bar2');
-            },
-        },
+        '@opentelemetry/instrumentation-http': {},
     })],
-    spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
+    spanProcessor: new BatchSpanProcessor(new TraceExporter()),
 })
 
 sdk.start()
